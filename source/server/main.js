06c80b0f7584e7264770473d77492ba407795e4b
Lukas Heise <lukas.heise@gmail.com>
2016-03-30 20:16:23 +0200

    .

--- source/server/main.js
+++ source/server/main.js
@@ -7,6 +7,7 @@
 (function () {
   "use strict";
 
+  var log4js = require('log4js');
   var express = require('express');
   var bodyParser = require('body-parser');
   // TODO: compression
@@ -15,6 +16,9 @@
   // TODO: node-uuid
 
   // TODO: do not accept posts larger than ??? bytes
+  // TODO: setup logger
+  var logger = log4js.getLogger('base');
+  logger.setLevel('INFO'); // TODO: fetch level from config
 
   var app = express();
 
@@ -34,5 +38,27 @@
   var port = process.argv[2] || 9050;
 
   app.listen(port);
-  console.log('Started application on port: ' + port);
+  logger.info('Started application on port: ' + port);
 }());
+
+/*
+  // HTTPS
+
+  var fs = require('fs');
+  var http = require('http');
+  var https = require('https');
+  var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+  var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+
+  var credentials = {key: privateKey, cert: certificate};
+  var express = require('express');
+  var app = express();
+
+  // your express configuration here
+
+  var httpServer = http.createServer(app);
+  var httpsServer = https.createServer(credentials, app);
+
+  httpServer.listen(80);
+  httpsServer.listen(443);
+*/
