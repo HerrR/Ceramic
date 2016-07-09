0abc3896af861b7af8fcababc185a3d38a02e441
Lukas Heise <lukas.heise@gmail.com>
2016-07-09 06:45:55 +0200

    .

--- gulp/tasks/ssh.js
+++ gulp/tasks/ssh.js
@@ -19,7 +19,7 @@ var gulpSSH = new ssh({
   sshConfig: {
     host: process.env.PUBLISH_URL,
     port: 22,
-    //privateKey: fs.readFileSync('~/.ssh/id_rsa'),
+    privateKey: fs.readFileSync(process.env.PUBLISH_IDRSA_PATH),
     username: process.env.PUBLISH_USER,
     passwod: process.env.PUBLISH_PASSWORD
   }
